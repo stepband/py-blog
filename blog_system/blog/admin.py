545af7c840032d055aa7fe31b6d7211c490e62f7
stepband <vladshvik@gmail.com>
2024-01-06 21:10:57 +0200

    Models, urls and basic view have been created

--- blog_system/blog/admin.py
+++ blog_system/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post, Comment, User
 
+from django.contrib.auth.models import Group
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(User)
+
+admin.site.unregister(Group)
 
