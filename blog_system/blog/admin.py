dac5b2a0b33c908c79d9fe652b0f54f76a2149cf
stepband <vladshvik@gmail.com>
2024-01-07 22:15:32 +0200

    technic commit

--- blog_system/blog/admin.py
+++ blog_system/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from django.contrib.auth.models import Group
 
+from .models import Post, Commentary, User
+
+admin.site.register(Post)
+admin.site.register(Commentary)
+admin.site.register(User)
+
+admin.site.unregister(Group)
